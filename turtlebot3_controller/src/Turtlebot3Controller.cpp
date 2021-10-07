3b968ceca4b3b9806b2b7a6c021e786cf007419a
Kent Howard Geraldo <kenthoward20@gmail.com>
2021-10-07 16:32:06 +0800

    adjust for workshop exercise

--- turtlebot3_controller/src/Turtlebot3Controller.cpp
+++ turtlebot3_controller/src/Turtlebot3Controller.cpp
@@ -42,46 +42,18 @@ bool Turtlebot3Controller::readParameters() {
 void Turtlebot3Controller::scanCallback(
 		const sensor_msgs::LaserScan::ConstPtr &msg) {
 	/* The code below uses C++ 11. The equivalent code using C++ 98 which is more non C++ user friendly is commented out
-    */
+    
     auto minDistanceIterator = std::min_element(msg->ranges.begin(), msg->ranges.end());
     auto minDistanceIndex = std::distance(msg->ranges.begin(), minDistanceIterator);
     const auto distance = *minDistanceIterator;
 	auto angle = msg->angle_min + msg->angle_increment * minDistanceIndex;
-	
+	*/
     
-
-
-    // float min = msg->range_max;
-    // float angle, distance;
-	// for (int i = 0; i < msg->ranges.size(); ++i) {
-	// 	if (msg->ranges[i] < min)
-	// 		{
-	// 		min = msg->ranges[i];
-	// 		angle = (msg->angle_min + msg->angle_increment*i);
-	// 		distance = msg->ranges[i];
-	// 		}
-    // }
-
-    publishVelocity(angle, distance);
-	prevAngle_ = angle;
-
-	if (angle*prevAngle_<0)
-		{
-			sumErrorAngle_= 0;
-		}
-	else
-		{
-			sumErrorAngle_ += angle;
-		}
     }
 
 
-void Turtlebot3Controller::publishVelocity(float angle, float distance){
-	velMsg_.linear.x = xVel_;
-	velMsg_.angular.z = kpAng_*angle + kiAng_*sumErrorAngle_ + kdAng_*(angle - prevAngle_); 
-	velPublisher_.publish(velMsg_);
-	ROS_INFO_STREAM_THROTTLE(2.0, "w_z: " << velMsg_.angular.z);
-    ROS_INFO_STREAM_THROTTLE(2.0, "e_theta: " << angle);
+void Turtlebot3Controller::publishVelocity(){
+
 }
 
 }
